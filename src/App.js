c2bc5fb3643337ddbb4b375ed55a6da19faf0b13
Melanie <melanie16203@gmail.com>
2022-02-17 07:09:06 -0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Sentence from './components/Sentence'
+import Spinner from './components/Spinner'
+
+import styled from '@emotion/styled'
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    padding-top: 5rem;
+    flex-direction: column;
+`
+
+const Button = styled.button`
+    background: -webkit-linear-gradient(
+        top left,
+        #007d35 0%,
+        #007d35 40%,
+        #0f574e 100%
+    );
+    background-size: 308px;
+    font-family: Arial, Helvetica, sans-serif;
+    color: #fff;
+    margin-top: 3rem;
+    padding: 1rem 3rem;
+    font-size: 2rem;
+    border: 2px solid #000;
+    cursor: pointer;
+    transition: background-size 0.8s ease;
+
+    &:hover {
+        background-size: 400px;
+    }
+`
+
+export default function App() {
+    const [sentence, setSentence] = useState({})
+    const [loading, setLoading] = useState(false)
+
+    const getSentence = async () => {
+        setLoading(true)
+
+        const response = await fetch(
+            'https://breaking-bad-quotes.herokuapp.com/v1/quotes'
+        )
+        const data = await response.json()
+        setSentence(data[0])
+
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        getSentence()
+    }, [])
+
+    return (
+        <Container>
+            {loading ? <Spinner /> : <Sentence sentence={sentence} />}
+            <Button onClick={getSentence}>Obtener Frase</Button>
+        </Container>
+    )
 }
-
-export default App;
